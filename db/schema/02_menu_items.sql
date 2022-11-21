7df5db50f34a20467b061124034c84d924708982
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-20 22:04:44 -0800

    Fix typo

--- db/schema/02_menu_items.sql
+++ db/schema/02_menu_items.sql
@@ -3,7 +3,7 @@
 DROP TABLE IF EXISTS menu_items CASCADE;
 CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
-  customer_id INTEGER REFERENCES users(id) NOT NULL,
+  customer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(50) NOT NULL,
   name VARCHAR(50) NOT NULL,
   image_url VARCHAR(255) NOT NULL,
