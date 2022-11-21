1cfdc104f9e962c05957f5cc6e2c13a998700cee
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-20 22:58:09 -0800

    Delete unecessary title column

--- db/schema/02_menu_items.sql
+++ db/schema/02_menu_items.sql
@@ -4,7 +4,6 @@ DROP TABLE IF EXISTS menu_items CASCADE;
 CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
   customer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  title VARCHAR(50) NOT NULL,
   name VARCHAR(50) NOT NULL,
   image_url VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
