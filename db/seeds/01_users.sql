ab9b72ef67adcb7f562f856ff3a711e405ef9b23
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-20 22:58:27 -0800

    Add mock data for users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,13 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, restaurant, phone)
+VALUES ('John Cena', false, '555-555-5555');
+INSERT INTO users (name, restaurant, phone)
+VALUES ('Guy Fierri', true, '444-555-5555');
+INSERT INTO users (name, restaurant, phone)
+VALUES ('Saul Goodman', false, '333-555-5555');
+INSERT INTO users (name, restaurant, phone)
+VALUES ('Dora The Explorer', true, '222-555-5555');
+INSERT INTO users (name, restaurant, phone)
+VALUES ('Jean-Chretien', false, '111-555-5555');
+INSERT INTO users (name, restaurant, phone)
+VALUES ('Helga Petaski', true, '000-555-5555');
