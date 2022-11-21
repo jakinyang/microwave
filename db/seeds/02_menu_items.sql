c6c757978fdcb6e9eed299ad2507925383174168
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-20 22:58:48 -0800

    Add mock data for menu_items table

---
+++ db/seeds/02_menu_items.sql
@@ -0,0 +1,50 @@
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (2, 'potatoes', 'https://thumbs.dreamstime.com/b/fresh-young-potato-sack-bag-isolated-white-39683115.jpg', '5lb bag of potatoes', 599, 'produce');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (2, 'potatoes', 'https://thumbs.dreamstime.com/b/fresh-young-potato-sack-bag-isolated-white-39683115.jpg', '5lb bag of potatoes', 599, 'produce');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (2, 'potatoes', 'https://thumbs.dreamstime.com/b/fresh-young-potato-sack-bag-isolated-white-39683115.jpg', '5lb bag of potatoes', 599, 'produce');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (2, 'gravy', 'https://www.shutterstock.com/image-photo/generic-tin-can-copy-space-260nw-137026637.jpg', '365ml gravy', 199, 'canned goods');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (2, 'gravy', 'https://www.shutterstock.com/image-photo/generic-tin-can-copy-space-260nw-137026637.jpg', '365ml can of gravy', 199, 'canned goods');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (2, 'pickled onions', 'https://thumbs.dreamstime.com/b/jar-pickled-onions-5630229.jpg', '1l jar of pickled onions', 699, 'canned goods');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (2, 'pickled onions', 'https://thumbs.dreamstime.com/b/jar-pickled-onions-5630229.jpg', '1l jar of pickled onions', 699, 'canned goods');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (4, 'potatoes', 'https://thumbs.dreamstime.com/b/fresh-young-potato-sack-bag-isolated-white-39683115.jpg', '5lb bag of potatoes', 299, 'produce');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (4, 'potatoes', 'https://thumbs.dreamstime.com/b/fresh-young-potato-sack-bag-isolated-white-39683115.jpg', '5lb bag of potatoes', 299, 'produce');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (4, 'peppers', 'https://thumbs.dreamstime.com/b/red-chili-peppers-sack-bag-white-background-61791331.jpg', '500g bag of jalapeno peppers', 399, 'produce');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (4, 'peppers', 'https://thumbs.dreamstime.com/b/red-chili-peppers-sack-bag-white-background-61791331.jpg', '500g bag of jalapeno peppers', 399, 'produce');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (4, 'tortillas', 'https://thumbs.dreamstime.com/b/flour-tortillas-18865913.jpg', '300g cooked tortillas', 199, 'bread');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (4, 'tortillas', 'https://thumbs.dreamstime.com/b/flour-tortillas-18865913.jpg', '300g cooked tortillas', 199, 'bread');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (4, 'beans', 'https://thumbs.dreamstime.com/b/open-tin-can-beans-isolated-white-41174357.jpg', '344g can of cooked beans', 99, 'canned goods');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (4, 'beans', 'https://thumbs.dreamstime.com/b/open-tin-can-beans-isolated-white-41174357.jpg', '344g can of cooked beans', 99, 'canned goods');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (4, 'beans', 'https://thumbs.dreamstime.com/b/open-tin-can-beans-isolated-white-41174357.jpg', '344g can of cooked beans', 99, 'canned goods');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (4, 'beans', 'https://thumbs.dreamstime.com/b/open-tin-can-beans-isolated-white-41174357.jpg', '344g can of cooked beans', 99, 'canned goods');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (6, 'sliced pork shoulder', 'https://thumbs.dreamstime.com/b/meat-packaging-tray-package-label-beef-polystyrene-72401853.jpg', '566g of packaged pork', 399, 'meat');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (6, 'sliced pork shoulder', 'https://thumbs.dreamstime.com/b/meat-packaging-tray-package-label-beef-polystyrene-72401853.jpg', '566g of packaged pork', 399, 'meat');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (6, 'sliced pork shoulder', 'https://thumbs.dreamstime.com/b/meat-packaging-tray-package-label-beef-polystyrene-72401853.jpg', '566g of packaged pork', 399, 'meat');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (6, 'ground turkey', 'https://thumbs.dreamstime.com/b/minced-chicken-styrofoam-packaging-supermarket-package-concept-wooden-table-255999412.jpg', '400g of ground turkey', 299, 'meat');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (6, 'ground turkey', 'https://thumbs.dreamstime.com/b/minced-chicken-styrofoam-packaging-supermarket-package-concept-wooden-table-255999412.jpg', '400g of ground turkey', 299, 'meat');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (6, 'assorted bread', 'https://www.publicdomainpictures.net/pictures/290000/nahled/fresh-baked-bread-in-the-bakery.jpg', '1kg of assorted bread', 499, 'bread');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (6, 'assorted bread', 'https://www.publicdomainpictures.net/pictures/290000/nahled/fresh-baked-bread-in-the-bakery.jpg', '1kg of assorted bread', 499, 'bread');
+INSERT INTO menu_items (customer_id, name, image_url, description, price, type)
+VALUES (6, 'maseca', 'https://i5.walmartimages.ca/images/Enlarge/914/475/999999-037297914475.jpg', '1kg of maseca', 199, 'flour');
