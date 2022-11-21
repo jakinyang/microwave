84787eb0d4787b545114437c15aa2c910a7e9947
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-20 21:39:59 -0800

    Correct typo

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,6 +5,6 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(50) NOT NULL,
   restaurant BOOLEAN DEFAULT false NOT NULL,
-  phone VARCHAR(50) NOT NULL,
+  phone VARCHAR(50) NOT NULL
   -- restaurant_id INTEGER REFERENCES restaurant(id);
 );
