1d3cc497d86be127896a877e360c215e1c6336b0
Russel Mueller <bohjaangles@gmail.com>
2022-11-23 00:17:37 +0000

    adding a function to restaurant-menu.js to populate menu items on restaurant page

--- public/scripts/restaurant-menu.js
+++ public/scripts/restaurant-menu.js
@@ -1,1 +1,38 @@
-const getOrders = require('')
+
+$(() => {
+  $.ajax({
+    method: 'GET',
+    url: 'api/restaurant/menu'
+  })
+  .done((response) => {
+    const $menuList = $('.listings-grid');
+    $menuList.empty();
+    for (const item of response.menu_items) {
+      $('.listings-grid').append(`<div class="listing-grid-element">
+          <div class="image">
+            <img
+              src="${item.image_url}"
+              alt="Listing Pic">
+          </div>
+          <div class="container-row">
+            <div class="text">
+              <h3>${item.name}</h3>
+              <div class="info">
+                <span>${item.price}</span><span>${item.stock}</span>
+              </div>
+            </div>
+            <div class="options">
+              <div class="container add-container">
+                <button class="options-btn selected">Add</button>
+              </div>
+              <div class="container add-container">
+                <button class="options-btn selected">Edit</button>
+              </div>
+            </div>
+          </div>
+        </div>`);
+    }
+  })
+});
+
+
