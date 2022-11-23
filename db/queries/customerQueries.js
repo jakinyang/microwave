202aa284c45c785de61efedd5131f0927795b7f4
Russel Mueller <bohjaangles@gmail.com>
2022-11-23 21:27:00 +0000

    adding helper functions to customer-menu.js and queries to customer-queries.js

--- db/queries/customerQueries.js
+++ db/queries/customerQueries.js
@@ -9,13 +9,40 @@ const getMenuItems = () => {
     });
 };
 
+const addMenuItemBasket = (menuItemObj) => {
+  console.log('addMenuItemsBaskets called');
+  const menuItemId = menuItemObj.id;
+  const basketId = 4;
+  const queryParams = [
+    menuItemId,
+    basketId
+  ]
+  return db.query(`
+  INSERT INTO menu_item_baskets (menu_item_id, basket_id)
+  VALUES ($1, $2)
+  RETURNING *;`, queryParams)
+  .then(res => {
+    return res.rows;
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+};
+
 const alterMenuItemStock = function(data) {
+  console.log('altermenuitems called');
+  const queryParams = [
+    2,
+    data.id,
+    data.stock
+  ]
   return db.query(`
   UPDATE menu_items
-  SET stock = ${data.stock},
-  WHERE menu_items.id = ${data.id};
-  `)
+  SET stock = $3,
+  WHERE menu_items.id = $2;
+  `, queryParams)
   .then(res => {
+    console.log('res from cust.querys :', res);
     return res.rows;
   })
   .catch((err) => {
@@ -31,4 +58,5 @@ const alterMenuItemStock = function(data) {
 module.exports = {
   getMenuItems,
   alterMenuItemStock,
+  addMenuItemBasket
 }
--- routes/customer-api.js
+++ routes/customer-api.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const db = require('../db/connection');
 const router  = express.Router();
-const { getMenuItems, alterMenuItemStock } = require('../db/queries/customerQueries');
+const { getMenuItems, alterMenuItemStock, addMenuItemBasket } = require('../db/queries/customerQueries');
 
-// Browse
+// BROWSE
 router.get('/menu', (req, res) => {
   const query = `SELECT * FROM menu_items WHERE restaurant_owner_id = 2;`;
   db.query(query)
@@ -18,11 +18,28 @@ router.get('/menu', (req, res) => {
     });
 });
 
-router.post('/menu', (req, res) => {
-  const newMenuObject = req.body;
-  addMenuItem(newMenuObject)
+
+// ADD
+router.post('/menu/basket', (req, res) => {
+  console.log('POST request receied to /menu/basket')
+  const menuItem = req.body;
+  addMenuItemBasket(menuItem)
   .then(response => {
     console.log(response);
+    res.send(response);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+})
+
+// EDIT
+router.post('/menu/stock/update', (req, res) => {
+  const newStockObject = req.body;
+  console.log('cust.api newStockObj :', newStockObject);
+  alterMenuItemStock(newStockObject)
+  .then(response => {
+    console.log('result from cust.api stock func :', response);
     res.send(response);
 
   })
